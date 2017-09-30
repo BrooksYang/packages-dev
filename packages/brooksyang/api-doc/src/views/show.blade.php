c69489eddbdc06b7c63f592fc24dbe9796c35d5a
BrooksYang <yangchangqing@easy2study.net>
2017-09-30 10:25:24 +0800

    form

--- packages/brooksyang/api-doc/src/views/show.blade.php
+++ packages/brooksyang/api-doc/src/views/show.blade.php
@@ -2,61 +2,74 @@
 
 @section('css')
     <style>
-        span {  width:60px; margin-right: 5px; }
-        td a { color: #363636; }
-        .button-custom {  width: 80px;  margin-bottom: 15px;  }
+        span {
+            width: 60px;
+            margin-right: 5px;
+        }
+
+        td a {
+            color: #363636;
+        }
+
+        .button-custom {
+            width: 80px;
+            margin-bottom: 15px;
+        }
     </style>
 @endsection
 
 @section('content')
-    <div class="">
-        <a href="javascript:history.back();" class="button button-custom">返回</a>
-        <a class="button is-outlined is-primary button-custom">测试</a>
-    </div>
+    <form action="">
+        <div class="">
+            <a href="javascript:history.back();" class="button button-custom">返回</a>
+            <button class="button is-outlined is-primary button-custom" type="submit">测试</button>
+        </div>
 
-    <div class="box">
-        <p>
-            <span class="tag is-rounded is-primary">HTTP</span><strong>{{ $info['name'] }}</strong>
-            <br>
+        <div class="box">
+            <p>
+                <span class="tag is-rounded is-primary">HTTP</span><strong>{{ $info['name'] }}</strong>
+                <br>
 
-            {{-- method --}}
-            @if ($info['method'] == 'GET')
-                <span class="tag is-rounded is-info">{{ $info['method'] }}</span>
-            @elseif ($info['method'] == 'POST')
-                <span class="tag is-rounded is-success">{{ $info['method'] }}</span>
-            @elseif (in_array($info['method'], ['PUT', 'PATCH']))
-                <span class="tag is-rounded is-warning">{{ $info['method'] }}</span>
-            @elseif ($info['method'] == 'DELETE')
-                <span class="tag is-rounded is-danger">{{ $info['method'] }}</span>
-            @endif
-            {{ $info['uri'] }}
-        </p>
-    </div>
+                {{-- method --}}
+                @if ($info['method'] == 'GET')
+                    <span class="tag is-rounded is-info">{{ $info['method'] }}</span>
+                @elseif ($info['method'] == 'POST')
+                    <span class="tag is-rounded is-success">{{ $info['method'] }}</span>
+                @elseif (in_array($info['method'], ['PUT', 'PATCH']))
+                    <span class="tag is-rounded is-warning">{{ $info['method'] }}</span>
+                @elseif ($info['method'] == 'DELETE')
+                    <span class="tag is-rounded is-danger">{{ $info['method'] }}</span>
+                @endif
+                {{ $info['uri'] }}
+            </p>
+        </div>
 
-    <div class="box">
-        <table class="table is-fullwidth">
-            <thead>
-            <tr>
-                <th class="">序号</th>
-                <th>参数</th>
-                <th>值</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach ($params as $key => $param)
+        <div class="box">
+            <table class="table is-fullwidth">
+                <thead>
                 <tr>
-                    <th>{{ $key + 1 }}</th>
-                    <td>{{ $param['param'] }}</td>
-                    <td>
-                        <input class="input" type="email" placeholder="{{ $param['comment'] }}">
-                    </td>
+                    <th class="">序号</th>
+                    <th>参数</th>
+                    <th>值</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </div>
+                </thead>
+                <tbody>
+                @foreach ($params as $key => $param)
+                    <tr>
+                        <th>{{ $key + 1 }}</th>
+                        <td>{{ $param['param'] }}</td>
+                        <td>
+                            <input class="input" type="email" placeholder="{{ $param['comment'] }}">
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
 
-    <div class="box">
-        返回结果：
-    </div>
+
+        <div class="box">
+            返回结果：
+        </div>
+    </form>
 @endsection
