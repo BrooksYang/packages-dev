51a56a69b9175e49f5892fde8536ed69f0875400
BrooksYang <yangchangqing@easy2study.net>
2017-09-30 14:14:03 +0800

    nav bar title

--- packages/brooksyang/api-doc/src/views/layouts/includes/nav_bar.blade.php
+++ packages/brooksyang/api-doc/src/views/layouts/includes/nav_bar.blade.php
@@ -1,8 +1,7 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
         <a class="navbar-item" href="{{ url('api/docs') }}">
-            <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox"
-                 width="112" height="28">
+            {{ config('app.name') }}
         </a>
 
         <button class="button navbar-burger">
